db01f59a8fb9bea2dcd68a5286ffbcffbaeda719
chen <yclooper@gmail.com>
2025-09-07 22:44:32 +0800

    init project

--- model.py
+++ model.py
@@ -1,9 +1,21 @@
+import os
+
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+base_url = os.getenv("OPENAI_API_BASE")
+
 
 def call_llm(prompt):
-    from openai import OpenAI
-    client = OpenAI(api_key="YOUR_DEEPSEEK_API_KEY", base_url="https://api.deepseek.com")
+    client = OpenAI(api_key=api_key, base_url=base_url)
     r = client.chat.completions.create(
         model="deepseek-chat",
         messages=[{"role": "user", "content": prompt}]
     )
     return r.choices[0].message.content
+
+
+if __name__ == '__main__':
+    print(call_llm("who are you?"))
